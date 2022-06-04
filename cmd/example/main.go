3b98e3cc6fa42e670fda059d0f96ff3d5fa95fb5
Oliver Berning <oliver_berning@yahoo.de>
2022-06-04 21:13:52 +0200

    Refactor for proper unit tests

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -5,9 +5,9 @@ import (
 )
 
 func main() {
-	conf := migrant.Config{
-		ConnectionUrl: "postgres://postgres:postgres@localhost:5432/migration_test",
-		FileLocation:  "./cmd/example/testdata",
+	db := migrant.Config{
+		Url:          "postgres://postgres:postgres@localhost:5432/migration_test",
+		FileLocation: "./cmd/example/testdata",
 	}
-	conf.Migrate()
+	db.Migrate()
 }
